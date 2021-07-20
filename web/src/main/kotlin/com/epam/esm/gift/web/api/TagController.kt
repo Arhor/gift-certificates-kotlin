7b4e65be29c8b21520e7f665eb3c24fd736b6afd
Maksim Buryshynets <maksim.buryshynets@ukg.com>
2021-07-20 17:11:45 +0300

    web-module: * added POST, DELETE end-points for the `tags` resource;

--- web/src/main/kotlin/com/epam/esm/gift/web/api/TagController.kt
+++ web/src/main/kotlin/com/epam/esm/gift/web/api/TagController.kt
@@ -2,10 +2,10 @@ package com.epam.esm.gift.web.api
 
 import com.epam.esm.gift.dto.TagDTO
 import com.epam.esm.gift.service.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/tags")
@@ -19,5 +19,23 @@ class TagController(private val service: Service<TagDTO, Long>) {
     @GetMapping("/{tagId}")
     fun getTagById(@PathVariable tagId: Long): TagDTO {
         return service.findOne(tagId)
+    }
+
+    @PostMapping
+    fun createNewTag(@RequestBody tag: TagDTO): ResponseEntity<*> {
+        val createdTag = service.create(tag)
+
+        val location =
+            ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .build(createdTag.id)
+
+        return ResponseEntity.created(location).body(createdTag)
+    }
+
+    @DeleteMapping("/{tagId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteTag(@PathVariable tagId: Long) {
+        service.deleteById(tagId)
     }
 }
